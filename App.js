8652306bcc8b1c49d6592f7e0cd34a15966e61ee
isaac545454 <isaac.newton1221@gmail.com>
2022-11-19 20:17:44 -0300

    init validadion form

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 export default function App() {
+  const handleSubmit = () => {
+    console.log("aa");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>bem vindo(a)</Text>
+      <View>
+        <TextInput placeholder="email" style={styles.input} />
+        <TextInput placeholder="senha" style={styles.input} />
+        <TextInput
+          placeholder="confirme a senha"
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <StatusBar style="auto" />
+      </View>
+      <TouchableOpacity style={styles.btnArea} onPress={handleSubmit}>
+        <Text style={styles.btnText}>Acessar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +35,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F0F8FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 25,
+    marginBottom: 20,
+  },
+  input: {
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    width: 290,
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+  },
+  btnArea: {
+    backgroundColor: "#32CD32",
+    paddingVertical: 14,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+  },
+  btnText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 15,
   },
 });
